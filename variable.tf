a9ddcab440773996069039ff390008fe52c749f4
harsha7979 <102642907+harsha7979@users.noreply.github.com>
2022-05-12 13:08:10 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,20 +3,16 @@ variable "region" {
 }
 
 variable "ami_id" {
-  type = "map"
-  default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn75gd33"
+    
+    default =  "ami-0022f774911c1d690"
   }
-}
 
-variable "key_name" {
-  type    = "string"
-  default = "key_name"
-}
+# variable "key_name" {
+#   type    = "string"
+#   default = "RDSKEY"
+# }
 
 variable "instance_type" {
-  type    = "string"
+  # type    = "string"
   default = "t2.micro"
 }
